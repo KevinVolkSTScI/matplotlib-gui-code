3efd4a8b9a66abcc1b2246c966aa05bc6ffc80de
Kevin Volk <37000208+KevinVolkSTScI@users.noreply.github.com>
2021-02-08 12:25:34 -0500

    Update setup.py
    Changed the version number.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("src/README.md", "r") as fh:
 
 setuptools.setup(
     name="matplotlib_user_interface", 
-    version="2020.11.19",
+    version="2021.02.08",
     author="Kevin Volk",
     author_email="kvolk@stsci.edu",
     description="A Matplotlib GUI program reminiscent of xmgrace.",
