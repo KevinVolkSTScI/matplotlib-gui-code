735f4476aaaa78f3334adc7b1b79bfb1cc9ec9f2
Kevin Volk <37000208+KevinVolkSTScI@users.noreply.github.com>
2021-05-08 12:57:47 -0400

    changed version number
    The version number has been updated since fits_image_display.py was updated.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("src/README.md", "r") as fh:
 
 setuptools.setup(
     name="matplotlib_user_interface", 
-    version="2021.04.07",
+    version="2021.05.08",
     author="Kevin Volk",
     author_email="kvolk@stsci.edu",
     description="A Matplotlib GUI program reminiscent of xmgrace.",
