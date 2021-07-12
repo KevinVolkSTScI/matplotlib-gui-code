13dc469bbbc70fec82c32133e15e8018d545083e
Kevin Volk <37000208+KevinVolkSTScI@users.noreply.github.com>
2021-07-12 10:42:01 -0400

    Add "j"/"k" key commands to fits_image_display.
    Add IRAF-like "j" and "k" key commands to the fits_image_display tool.  Also small revision to mpfit.py/mpfitexpr.py which are used to make Gaussian fits to the extracted profiles from the key commands.  Uses pyplot for the segment plots for the keys.

--- src/mpfitexpr.py
+++ src/mpfitexpr.py
@@ -20,48 +20,48 @@ import re
 import numpy
 
 def mpfitexpr(func, x, y, err , start_params, check=True, full_output=False, **kw):
-	"""Fit the used defined expression to the data
-	Input:
-	- func: string with the function definition 
-	- x: x vector
-	- y: y vector
-	- err: vector with the errors of y
-	- start_params: the starting parameters for the fit
-	Output:
-	- The tuple (params, yfit) with best-fit params and the values of func evaluated at x
-	Keywords:
-	- check: boolean parameter. If true(default) the function will be checked for sanity
-	- full_output: boolean parameter. If True(default is False) then instead of best-fit parameters the mpfit object is returned
-	Example:
-	params,yfit=mpfitexpr('p[0]+p[2]*(x-p[1])',x,y,err,[0,10,1])
+    """Fit the used defined expression to the data
+    Input:
+    - func: string with the function definition 
+    - x: x vector
+    - y: y vector
+    - err: vector with the errors of y
+    - start_params: the starting parameters for the fit
+    Output:
+    - The tuple (params, yfit) with best-fit params and the values of func evaluated at x
+    Keywords:
+    - check: boolean parameter. If true(default) the function will be checked for sanity
+    - full_output: boolean parameter. If True(default is False) then instead of best-fit parameters the mpfit object is returned
+    Example:
+    params,yfit=mpfitexpr('p[0]+p[2]*(x-p[1])',x,y,err,[0,10,1])
 	
-	If you need to use numpy functions in your function, then
-		you must to use the full names of these functions, e.g.:
-		numpy.sin, numpy.cos etc.
+    If you need to use numpy functions in your function, then
+    you must to use the full names of these functions, e.g.:
+    numpy.sin, numpy.cos etc.
 	
-	This function is motivated by mpfitexpr() from wonderful MPFIT IDL package
-		written by Craig Markwardt	
+    This function is motivated by mpfitexpr() from wonderful MPFIT IDL package
+    written by Craig Markwardt	
 	
-	"""
+    """
 
-	def myfunc(p,fjac=None,x=None, y=None, err=None):
-		return [0, eval('(y-(%s))/err'%func)]
+    def myfunc(p,fjac=None,x=None, y=None, err=None):
+        return [0, eval('(y-(%s))/err'%func)]
 
-	myre = "[^a-zA-Z]p\[(\d+)\]"
-	r = re.compile(myre)
-	maxp = -1
-	for m in re.finditer(r,func):
-		curp = int(m.group(1))
-		maxp = curp if curp > maxp else maxp	
-	if check:
-		if maxp == -1: 
-			raise Exception("wrong function format")
-		if maxp + 1 != len(start_params):
-			raise Exception("the length of the start_params != the length of the parameter verctor of the function")
-	fa={'x' : x, 'y' : y,'err' : err}
-	res = mpfit.mpfit(myfunc,start_params,functkw=fa,**kw)
-	yfit = eval(func, globals(), {'x':x, 'p': res.params})
-	if full_output:
-		return (res, yfit)
-	else:
-		return (res.params, yfit)
+    myre = "[^a-zA-Z]p\[(\d+)\]"
+    r = re.compile(myre)
+    maxp = -1
+    for m in re.finditer(r,func):
+        curp = int(m.group(1))
+        maxp = curp if curp > maxp else maxp	
+    if check:
+        if maxp == -1: 
+            raise Exception("wrong function format")
+        if maxp + 1 != len(start_params):
+            raise Exception("the length of the start_params != the length of the parameter verctor of the function")
+    fa={'x' : x, 'y' : y,'err' : err}
+    res = mpfit.mpfit(myfunc,start_params,functkw=fa,**kw)
+    yfit = eval(func, globals(), {'x':x, 'p': res.params})
+    if full_output:
+        return (res, yfit)
+    else:
+        return (res.params, yfit)
